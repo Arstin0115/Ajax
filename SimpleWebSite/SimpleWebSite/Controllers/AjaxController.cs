321c57b78c442b3da44c5654e7711433d015910a
pandariceball <tealove6053@gmail.com>
2023-02-20 16:40:11 +0800

    Ajax0220

---
+++ SimpleWebSite/SimpleWebSite/Controllers/AjaxController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebSite.Models;
+using System.Xml.Linq;
+
+namespace SimpleWebSite.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private NorthwindContext _context;
+
+        public AjaxController (NorthwindContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string Greet(string Name)
+        {
+            Thread.Sleep(3000);
+            return $"Hello {Name}";
+        }
+
+        [HttpPost,ActionName("Greet")]
+        public string PostGreet(string Name)
+        {
+            return $"Hello {Name}";
+        }
+
+        [HttpPost]
+        public string FetchPostGreet([FromBody]Parameter p)
+        {
+            return $"Hello {p.Name}";
+        }
+
+        [HttpPost]
+        public string CheckName(string FirstName)
+        {
+            bool Exists = _context.Employees.Any(e => e.FirstName == FirstName);
+
+            return Exists ? "true" : "false" ;
+        }
+
+        [HttpPost]
+        public string FetchCheckName([FromBody]Employees emp)
+        {
+            bool Exists = _context.Employees.Any(e => e.FirstName == emp.FirstName);
+
+            return Exists ? "true" : "false";
+        }
+
+
+    }
+}
---
+++ SimpleWebSite/SimpleWebSite/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebSite.Models;
+using System.Diagnostics;
+
+namespace SimpleWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Greet()
+        {
+            return View();
+        }
+
+
+        public IActionResult jQueryGreet()
+        {
+            return View();
+        }
+
+        public IActionResult FetchGreet()
+        {
+            return View();
+        }
+
+        public IActionResult CheckName()
+        {
+            return View();
+        }
+
+        public IActionResult jQueryCheckName()
+        {
+            return View();
+        }
+
+        public IActionResult FetchCheckName()
+        {
+            return View();
+        }
+    }
+}
